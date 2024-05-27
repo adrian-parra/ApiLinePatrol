b6283c3cceb3829f60098c22947410e301a818ca
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-05-26 21:22:50 -0700

    feat retornar datos de voseos de prueba

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 
 import linePatrolRoutes  from './routes/linePatrol.routes.js'
+import voseoRoutes from './routes/voseo.routes.js'
 
 const app = express()
 
@@ -9,6 +10,7 @@ const app = express()
 app.use(express.json())
 
 app.use("/api", linePatrolRoutes)
+app.use("/api", voseoRoutes)
 
 app.listen(3000)
 console.log('server en port 3000')
