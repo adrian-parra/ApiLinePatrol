2c67f0abfb98516f70315c74ca43ac3b98507933
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-10-10 09:55:02 -0700

    change(feat cmd)
    * Add endpoint para apagar equipo wmi
    
    * Add endpoint para obtener informacion de equipo wmmi
    
    * Add endpoint para obtener ultimna vez que se encendio equipo wmi

--- src/routes/cmd.routes.js
+++ src/routes/cmd.routes.js
@@ -4,7 +4,178 @@ import { exec } from "child_process"
 
 const router = Router();
 
+// APAGAR MAQUINA CON WMI
+router.post('/cmd/wmi/ShutdownDevice', async (req, res) => {
+  const remoteComputer = req.body.ip;
+  try{
+    const command = `powershell -Command "Stop-Computer -ComputerName '${remoteComputer}' -Force"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al apagar el equipo: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      console.log(`Resultados: ${stdout}`);
+      return res.json("Equipo apagado con éxito");
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
 
+
+
+
+
+
+/* ESTA RUTA OBTIENE EL TIEMPO QUE HA TENIDO PRENDIDA UNA PC */
+router.post('/cmd/wmi/uptime', async (req, res) => {
+  const remoteComputer = req.body.ip;
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_OperatingSystem -ComputerName '${remoteComputer}' | Select-Object -Property LastBootUpTime"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener el tiempo de inicio: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+
+      const formatFechaDeEncendido = formatFechaDeEncendidoPc(stdout);
+      const calcularTiempoDeEncendido = calcularTiempoDeEncendidoPc(stdout)
+
+      console.log(`Resultados: ${calcularTiempoDeEncendido}`);
+      return res.json({fechaEncendido:formatFechaDeEncendido,tiempoEncendido:calcularTiempoDeEncendido});
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+/* ESTA RUTA OBTIENE EL NOMBRE DE LA PC REMOTA */
+router.post('/cmd/wmi/hostname', async (req, res) => {
+  const remoteComputer = req.body.ip;
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_ComputerSystem -ComputerName '${remoteComputer}' | Select-Object -Property Name"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener el nombre del equipo: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      console.log(`Resultados: ${stdout}`);
+      return res.json(stdout);
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+/* ESTA RUTA OBTIENE LA VERSIÓN DEL SISTEMA OPERATIVO DE LA PC REMOTA */
+router.post('/cmd/wmi/os/version', async (req, res) => {
+  const remoteComputer = req.body.ip;
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_OperatingSystem -ComputerName '${remoteComputer}' | Select-Object -Property Version"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener la versión del sistema operativo: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      console.log(`Resultados: ${stdout}`);
+      return res.json(stdout);
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+/* ESTA RUTA OBTIENE EL NOMBRE DEL PRODUCTO DE LA PC REMOTA */
+router.post('/cmd/wmi/product/name', async (req, res) => {
+  const remoteComputer = req.body.ip;
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_ComputerSystem -ComputerName '${remoteComputer}' | Select-Object -Property Name"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener el nombre del producto: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      console.log(`Resultados: ${stdout}`);
+      return res.json(stdout);
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+/* ESTA RUTA OBTIENE LA INFORMACIÓN DE LA PC REMOTA */
+router.post('/cmd/wmi/info', async (req, res) => {
+  const remoteComputer = req.body.ip
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_ComputerSystem -ComputerName '${remoteComputer}' | Select-Object -Property * | Out-String"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener la información del equipo: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      
+      
+      return res.json(formatInfoPc(stdout));
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+/* ESTA RUTA OBTIENE LA INFORMACIÓN DE LA PC REMOTA */
+router.post('/cmd/wmi/info/short', async (req, res) => {
+  const remoteComputer = req.body.ip
+  try{
+    const command = `powershell -Command "Get-WmiObject -Class Win32_ComputerSystem -ComputerName '${remoteComputer}' | Select-Object -Property Name, Manufacturer, Model, SystemType, TotalPhysicalMemory, Processor, OperatingSystem, Version, LastBootUpTime | Out-String"`;
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error al obtener la información del equipo: ${error.message}`);
+        return res.status(500).json(error.message );
+      }
+      if (stderr) {
+        console.error(`Error en stderr: ${stderr}`);
+        return res.status(500).json(stderr );
+      }
+      console.log(`Resultados: ${stdout}`);
+      return res.json(stdout);
+    });
+  }catch(error){
+    console.error(`Error: ${error.message}`);
+    res.status(500).json({ error: error.message });
+  }
+})
+
+//! FIN IA
 
 /*NO SE TERMINO DE CREAR LOGICA YA QUE NO HE ENCONTRADO COMO EJECUTAR UN SCRIPT COMPLETO SOLO COMANDOS... */
 router.post('/cmd/wmi/apps/close/duplicate', async (req, res) => {
@@ -229,5 +400,80 @@ router.post('/cmd', (req, res) => {
 
 
 })
+
+
+const formatInfoPc = (info) => {
+  const rawData = info
+// Limpiar y formatear la salida
+const formattedData = rawData
+  .split('\r\n')
+  .filter(line => line.trim() !== '') // Eliminar líneas vacías
+  .map(line => line.trim()) // Eliminar espacios en blanco al inicio y al final
+  .map(line => {
+    const [key, value] = line.split(':').map(part => part.trim());
+    return `${key}: ${value}`;
+  })
+  .join('\n');
+
+console.log("Información del Sistema:\n");
+console.log(formattedData);
+return formattedData
+
+}
+
+const formatFechaDeEncendidoPc = (rawBootTimeP)=>{
+  const rawBootTime = rawBootTimeP.trim();
+
+// Extraer la cadena de tiempo
+const bootTimeString = rawBootTime.match(/\d{14}/)[0];
+
+// Convertir la cadena a un objeto Date
+const year = parseInt(bootTimeString.substring(0, 4), 10);
+const month = parseInt(bootTimeString.substring(4, 6), 10) - 1; // Los meses en JavaScript son 0-indexados
+const day = parseInt(bootTimeString.substring(6, 8), 10);
+const hour = parseInt(bootTimeString.substring(8, 10), 10);
+const minute = parseInt(bootTimeString.substring(10, 12), 10);
+const second = parseInt(bootTimeString.substring(12, 14), 10);
+
+// Crear un objeto Date
+const bootDate = new Date(year, month, day, hour, minute, second);
+
+// Formatear la fecha a un formato legible
+const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', timeZone: 'UTC' };
+const formattedBootTime = bootDate.toLocaleString('es-ES', options);
+
+//console.log(`La PC fue encendida por última vez el: ${formattedBootTime}`);
+return formattedBootTime
+}
+
+const calcularTiempoDeEncendidoPc = (rawBootTimeP)=>{
+  const rawBootTime = rawBootTimeP.trim();
+
+// Extraer la cadena de tiempo
+const bootTimeString = rawBootTime.match(/\d{14}/)[0];
+
+// Convertir la cadena a un objeto Date
+const year = parseInt(bootTimeString.substring(0, 4), 10);
+const month = parseInt(bootTimeString.substring(4, 6), 10) - 1; // Los meses en JavaScript son 0-indexados
+const day = parseInt(bootTimeString.substring(6, 8), 10);
+const hour = parseInt(bootTimeString.substring(8, 10), 10);
+const minute = parseInt(bootTimeString.substring(10, 12), 10);
+const second = parseInt(bootTimeString.substring(12, 14), 10);
+
+// Crear un objeto Date
+const bootDate = new Date(year, month, day, hour, minute, second);
+
+// Calcular el tiempo de actividad
+const uptime = new Date() - bootDate; // Tiempo en milisegundos
+
+// Convertir milisegundos a días, horas y minutos
+const days = Math.floor(uptime / (1000 * 60 * 60 * 24));
+const hours = Math.floor((uptime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+const minutes = Math.floor((uptime % (1000 * 60 * 60)) / (1000 * 60));
+
+//console.log(`La PC lleva encendida: ${days} días, ${hours} horas y ${minutes} minutos.`);
+return `${days} días, ${hours} horas y ${minutes} minutos.`
+
+}
 
 export default router;
