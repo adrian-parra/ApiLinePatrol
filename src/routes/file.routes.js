74dea5b9cce2994ec1524f7f8a832e1b4e64b920
adrian-parra <adrian.adrianborquez@hotmail.com>
2024-12-15 19:37:58 -0700

    change(feat fileUpload)
    * add endpoint para eliminar archivos
    
    * add atributo isProtected en modelo file para proteger algunos archivos de ser eliminados
    
    * add unique key a atributo filePath en model file

--- src/routes/file.routes.js
+++ src/routes/file.routes.js
@@ -13,7 +13,7 @@ router.get('/files', async (req, res) => {
   });
   
   // Endpoint para registrar un archivo
-  router.post('/files', async (req, res) => {
+router.post('/files', async (req, res) => {
     const { fileName, filePath } = req.body;
     if (!fileName || !filePath) {
       return res.status(400).send('Faltan datos');
@@ -25,6 +25,39 @@ router.get('/files', async (req, res) => {
   
     res.json(newFile);
   });
+
+router.put('/files', async (req, res) => {
+    const { filePath } = req.body;
+    console.log(filePath)
+    if (!filePath) {
+      return res.status(400).send('Faltan datos');
+    }
+  
+    try {
+
+      // Verificar si el archivo existe y si está protegido
+      const file = await prisma.file.findUnique({
+        where: { filePath },
+    });
+
+    if (!file) {
+      return res.status(404).send('El archivo no fue encontrado');
+  }
+
+  if (file.isProtected) {
+      return res.status(403).send('Este archivo está protegido y no se puede eliminar');
+  }
+
+      const fileDeleted = await prisma.file.delete({
+        where: { filePath },
+      });
+      res.json(fileDeleted);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error al eliminar el archivo' });
+    }
+  
+})
 
 
 export default router;
